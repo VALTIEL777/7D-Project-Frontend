086192740b3c1be731c92470cb164a07240a2158
VALTIEL777 <elianmedinacobos@gmail.com>
2025-05-24 15:11:22 -0600

    Add login interfaces and Materials module

---
+++ 7d-compass/src/app/features/login/log-out/log-out.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MATERIAL_MODULES } from '../../../material';
+
+@Component({
+  selector: 'app-log-out',
+  imports: [MATERIAL_MODULES],
+  templateUrl: './log-out.component.html',
+  styleUrl: './log-out.component.scss'
+})
+export class LogOutComponent {
+
+}
---
+++ 7d-compass/src/app/features/login/reset-password/reset-password.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MATERIAL_MODULES } from '../../../material';
+
+@Component({
+  selector: 'app-reset-password',
+  imports: [MATERIAL_MODULES],
+  templateUrl: './reset-password.component.html',
+  styleUrl: './reset-password.component.scss'
+})
+export class ResetPasswordComponent {
+
+}
