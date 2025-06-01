d277275108c1b59621f9ea941b333fcc2d7a42e6
Christba03 <christba455@gmail.com>
2025-06-01 01:35:03 -0600

    feat: dashboardlayout

--- 7d-compass/src/app/shared/dashboard-layout/dashboard-layout.component.ts
+++ 7d-compass/src/app/shared/dashboard-layout/dashboard-layout.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { SideNavbarComponent } from '../side-navbar/side-navbar.component';
-import { RouterOutlet } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
 import { TitleBarComponent } from "../title-bar/title-bar.component";
 @Component({
   selector: 'app-dashboard-layout',
-  imports: [SideNavbarComponent, TitleBarComponent],
+  imports: [SideNavbarComponent, TitleBarComponent, MatCardModule],
   templateUrl: './dashboard-layout.component.html',
   styleUrl: './dashboard-layout.component.scss'
 })
