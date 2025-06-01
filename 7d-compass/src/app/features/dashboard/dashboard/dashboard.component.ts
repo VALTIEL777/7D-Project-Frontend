1e4d5e7384c822f606cb779427c796676c2730f5
Christba03 <christba455@gmail.com>
2025-06-01 02:57:20 -0600

    feat: botton card

--- 7d-compass/src/app/features/dashboard/dashboard/dashboard.component.ts
+++ 7d-compass/src/app/features/dashboard/dashboard/dashboard.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 import { DashboardLayoutComponent } from '../../../shared/dashboard-layout/dashboard-layout.component';
+import { CardWithButtonComponent } from "../../../shared/card-with-button/card-with-button.component";
 @Component({
   selector: 'app-dashboard',
-  imports: [DashboardLayoutComponent],
+  imports: [DashboardLayoutComponent, CardWithButtonComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
