12bfdf4d462f86274027d3d4512d040c99395501
VALTIEL777 <elianmedinacobos@gmail.com>
2025-05-31 18:43:09 -0600

    feat: agrega funcionalidad al login

--- 7d-compass/src/app/app.routes.ts
+++ 7d-compass/src/app/app.routes.ts
@@ -4,12 +4,16 @@ import { LogOutComponent } from './features/login/log-out/log-out.component';
 import { ForgotPasswordComponent } from './features/login/forgot-password/forgot-password.component';
 import { ResetPasswordComponent } from './features/login/reset-password/reset-password.component';
 import { ConfirmEmailComponent } from './features/login/confirm-email/confirm-email.component';
+import { DashboardComponent } from './features/dashboard/dashboard/dashboard.component';
 
 export const routes: Routes = [
     { path: '', component: LoginComponent },
+    { path: 'login', component: LoginComponent },
     { path: 'logout', component: LogOutComponent },
-    { path: 'forgot-password', component: ForgotPasswordComponent },
+    { path: 'forgotpassword', component: ForgotPasswordComponent },
     { path: 'reset-password', component: ResetPasswordComponent },
     { path: 'confirm-email', component: ConfirmEmailComponent },
+    { path: 'dashboard', component: DashboardComponent },
+
 
 ];
---
+++ 7d-compass/src/app/core/services/auth.service.ts
@@ -0,0 +1,41 @@
+// core/services/auth.service.ts
+import { Injectable } from '@angular/core';
+import { Observable, of, throwError } from 'rxjs';
+import { delay } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  // Datos dummy
+  private dummyUser = {
+    username: 'admin',
+    email: 'elianmedina@gmail.com',
+    password: '123456',
+    token: 'dummy-token-abc123'
+  };
+
+  login(credentials: { email?: string; username?: string; password: string }): Observable<any> {
+    const { email, username, password } = credentials;
+
+    const valid =
+      (email === this.dummyUser.email || username === this.dummyUser.username) &&
+      password === this.dummyUser.password;
+
+    if (valid) {
+      return of({ token: this.dummyUser.token, user: this.dummyUser }).pipe(delay(1000));
+    } else {
+      return throwError(() => ({ error: { message: 'Credenciales incorrectas' } }));
+    }
+  }
+
+  handleLoginResponse(response: any): void {
+    localStorage.setItem('token', response.token);
+    console.log('Token guardado:', response.token);
+  }
+
+  isLoggedIn(): boolean {
+  return !!localStorage.getItem('token');
+}
+
+}
