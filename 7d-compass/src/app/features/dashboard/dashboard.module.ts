12de33e82b8a67cd523afff9ad744093aee986c1
VALTIEL777 <elianmedinacobos@gmail.com>
2025-06-02 21:58:57 -0600

    feat: agrega interfaces fines-penalties y rts-processing

--- 7d-compass/src/app/features/dashboard/dashboard.module.ts
+++ 7d-compass/src/app/features/dashboard/dashboard.module.ts
@@ -2,9 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { CommonModule } from '@angular/common'; 
+import { MiscGenerationComponent } from './rtr-data/misc-generation/misc-generation.component';
+import { RtrProcessingComponent } from './rtr-data/rtr-processing/rtr-processing.component';
 
 const routes: Routes = [
-  { path: '', component: DashboardComponent }
+  { path: '', component: DashboardComponent },
+  //RTR DATA
+    { path: 'misc', component: MiscGenerationComponent },
+    { path: 'rtr.processiong', component: RtrProcessingComponent }
+
+
 ];
 
 @NgModule({
