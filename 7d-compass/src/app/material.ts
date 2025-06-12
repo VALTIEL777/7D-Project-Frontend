23f6265f1cd1e8bddc2be9eb66b4a6e0fa40bdb0
VALTIEL777 <elianmedinacobos@gmail.com>
2025-06-12 03:38:14 -0600

    feat: agrega MatDatepickerModule y  MatNativeDateModule

--- 7d-compass/src/app/material.ts
+++ 7d-compass/src/app/material.ts
@@ -8,6 +8,8 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatTableModule } from '@angular/material/table';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 
 
 export const MATERIAL_MODULES = [
@@ -20,5 +22,7 @@ export const MATERIAL_MODULES = [
   MatSelectModule,
   MatSidenavModule,
   MatTableModule,
-  MatSlideToggleModule
+  MatSlideToggleModule,
+   MatDatepickerModule,
+  MatNativeDateModule
 ];
