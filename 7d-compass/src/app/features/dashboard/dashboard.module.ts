7134319af57b30f4f9c5df49b3d2392c99f30c72
Christba03 <christba455@gmail.com>
2025-07-15 21:18:13 -0600

    Update dashboard.module.ts
    for change on crew genration

--- 7d-compass/src/app/features/dashboard/dashboard.module.ts
+++ 7d-compass/src/app/features/dashboard/dashboard.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { CommonModule } from '@angular/common'; 
-import { MiscGenerationComponent } from './rtr-data/misc-generation/misc-generation.component';
+import { CommonModule } from '@angular/common';
+import { CrewGenerationComponent } from './rtr-data/crew-generation/crew-generation.component';
 import { RtrProcessingComponent } from './rtr-data/rtr-processing/rtr-processing.component';
 
 const routes: Routes = [
   { path: '', component: DashboardComponent },
   //RTR DATA
-    { path: 'misc', component: MiscGenerationComponent },
-    { path: 'rtr.processiong', component: RtrProcessingComponent }
+    { path: 'crew-generation', component: CrewGenerationComponent },
+    { path: 'rtr-processing', component: RtrProcessingComponent }
 
 
 ];
