0139a5f40d33a3004b133ec3c399f5de5337be8d
Christba03 <christba455@gmail.com>
2025-07-28 19:24:59 -0600

    fat: gallery preview

--- 7d-compass/src/app/features/dashboard/dashboard.module.ts
+++ 7d-compass/src/app/features/dashboard/dashboard.module.ts
@@ -4,21 +4,22 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { CommonModule } from '@angular/common';
 import { CrewGenerationComponent } from './rtr-data/crew-generation/crew-generation.component';
 import { RtrProcessingComponent } from './rtr-data/rtr-processing/rtr-processing.component';
+import { PhotoEvidenceComponent } from './photo-evidence/photo-evidence.component';
 
 const routes: Routes = [
   { path: '', component: DashboardComponent },
   //RTR DATA
     { path: 'crew-generation', component: CrewGenerationComponent },
-    { path: 'rtr-processing', component: RtrProcessingComponent }
-
-
+    { path: 'rtr-processing', component: RtrProcessingComponent },
+    { path: 'photo-evidence', component: PhotoEvidenceComponent }
 ];
 
 @NgModule({
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
-    DashboardComponent
+    DashboardComponent,
+    PhotoEvidenceComponent
   ]
 })
 export class DashboardModule { }
